a1b7ca2456087552774d09f8189777a6a174734e
bachmeyer <wesyb@yahoo.com>
2016-01-20 13:22:11 -0600

    initial upload of constructor usage in csharp

---
+++ csharpconstructors/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpconstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address mikesAddress = new Address()
+            {
+                Street = "55555 Place Rd",
+                City = "Houston",
+                State = "TX",
+                Zipcode = "77070"
+            };
+
+            Customer mike = new Customer()
+            {
+                FirstName = "Mike",
+                LastName = "Harrison",
+                BillingAddress = mikesAddress,
+                ShippingAddress = mikesAddress
+            };
+
+        }
+    }
+}
---
+++ csharpconstructors/products.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpconstructors
+{
+    class Products
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int UnitsInStock { get; set; }
+        public string Description { get; set; }
+
+        public Products(string name, decimal price, int units)
+        {
+            Name = name;
+            Price = price;
+            UnitsInStock = units;
+        }
+        public Products(string name, decimal price, int units, string desc)
+        {
+            Name = name;
+            Price = price;
+            UnitsInStock = units;
+            Description = desc;
+        }
+
+    }
+}
